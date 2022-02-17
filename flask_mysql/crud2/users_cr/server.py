1f4db7f0776f7a3f6a05f14c3e8c11a27d8fa8dc
Jared McCoon <jaredmccoon@protonmail.com>
2022-02-16 17:10:58 -0800

    users crud

--- flask_mysql/crud2/users_cr/server.py
+++ flask_mysql/crud2/users_cr/server.py
@@ -14,7 +14,7 @@ def users():
     return render_template("read.html",users=User.get_all())
 
 
-@app.route('/user/new')
+@app.route('/users/new')
 def new():
     return render_template("create.html")
 
@@ -24,6 +24,32 @@ def create():
     User.save(request.form)
     return redirect('/users')
 
+@app.route('/users/<int:id>')
+def showuser(id):
+    data ={ 
+        "id":id
+    }
+    return render_template("user.html",user=User.get_one(data))
+
+@app.route('/users/<int:id>/edit')
+def edit(id):
+    data ={ 
+        "id":id
+    }
+    return render_template("edituser.html",user=User.get_one(data))
+
+@app.route('/users/update', methods=['POST'])
+def update():
+    User.update(request.form)
+    return redirect('/users')
+
+@app.route('/users/delete/<int:id>')
+def destroy(id):
+    data ={
+        'id': id
+    }
+    User.destroy(data)
+    return redirect('/users')
 
 if __name__=="__main__":
     app.run(debug=True)
--- flask_mysql/crud2/users_cr/users.py
+++ flask_mysql/crud2/users_cr/users.py
@@ -10,6 +10,9 @@ class User:
         self.updated_at = data["updated_at"]
 
 
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
@@ -26,3 +29,20 @@ class User:
         # comes back as the new row id
         result = connectToMySQL('users').query_db(query,data)
         return result
+
+    @classmethod
+    def get_one(cls,data):
+        query  = "SELECT * FROM users WHERE id = %(id)s;"
+        result = connectToMySQL('users').query_db(query,data)
+        return cls(result[0])
+
+    @classmethod
+    def update(cls,data):
+        query = "UPDATE users SET first_name=%(first_name)s,last_name=%(last_name)s,email=%(email)s,updated_at=NOW() WHERE id = %(id)s;"
+        return connectToMySQL('users').query_db(query,data)
+
+
+    @classmethod
+    def destroy(cls,data):
+        query  = "DELETE FROM users WHERE id = %(id)s;"
+        return connectToMySQL('users').query_db(query,data)
